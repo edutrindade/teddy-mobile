2c3b95704e51efe5adb8ec68e5693ff1f647a582
Eduardo Trindade <eduardoepalmeiras@gmail.com>
2025-05-30 23:59:32 -0300

    feat: tipagem inicial para cores da aplicação

---
+++ src/presentation/styles/colors.ts
@@ -0,0 +1,11 @@
+export const colors: TColors = {
+  primary: '#EC6724',
+  white: '#FFFFFF',
+  light: '#F5F5F5',
+  light200: '#D9D9D9',
+  grey: '#AAAAAA',
+  black: '#000000',
+  black100: 'rgba(0,0,0,0.4)',
+  error: '#FFB2B2',
+  transparent: 'transparent',
+};
