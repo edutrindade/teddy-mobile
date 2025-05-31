b4f0f2ba970e3eab3bc448f6ec33100ad9b774db
Eduardo Trindade <eduardoepalmeiras@gmail.com>
2025-05-31 11:54:10 -0300

    feat: ajustes na Home para navegação e abertura do teclado

--- src/presentation/screens/Home/index.tsx
+++ src/presentation/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 import { CommonText as Text } from '@/presentation/components/CommonText';
 
 import styles from './styles';
@@ -6,17 +6,22 @@ import { layout } from '@/presentation/styles/layout';
 import { Input } from '@/presentation/components/Input';
 import { useState } from 'react';
 import Button from '@/presentation/components/Button';
+import { useNavigation } from '@react-navigation/native';
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
   const [name, setName] = useState('');
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS == 'android' ? 'height' : 'padding'}
+    >
       <Text style={{ fontSize: layout.fontSize.title }}>Olá, seja bem-vindo!</Text>
 
       <Input onChangeText={setName} value={name} placeholder="Digite seu nome" />
 
-      <Button title="Entrar" onPress={() => alert(`Olá, ${name}!`)} />
-    </View>
+      <Button title="Entrar" onPress={() => navigation.navigate('Customers')} disabled={!name} />
+    </KeyboardAvoidingView>
   );
 }
