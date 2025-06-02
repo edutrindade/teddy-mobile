e3ba78e0de582b6983367602ff2d07334ae50988
Eduardo Trindade <eduardoepalmeiras@gmail.com>
2025-06-02 09:31:02 -0300

    chore: instalação da lib BottomSheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import 'react-native-reanimated';
 import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 import Routes from './presentation/routes';
 
@@ -23,7 +25,9 @@ export default function App() {
 
   return (
     <SafeAreaProvider>
-      <Routes />
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Routes />
+      </GestureHandlerRootView>
     </SafeAreaProvider>
   );
 }
